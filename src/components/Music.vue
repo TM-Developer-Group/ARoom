edac9ac48f8b7ddb5c373fc66ffc646df141e820
Cit1zeN4 <set4433@gmail.com>
2020-01-14 22:28:32 +0300

    Added MusicSongList

--- src/components/Music.vue
+++ src/components/Music.vue
@@ -6,29 +6,70 @@
       </div>
     </div>
     <div class="row d-flex justify-content-center align-items-baseline">
-      <a class="selector">songs</a>
+      <a class="selector selector-active">songs</a>
       <a class="selector">albums</a>
       <a class="selector">artists</a>
       <a class="selector">playlists</a>
       <a class="selector">filter</a>
     </div>
     <hr />
+    <MusicSongList :songList="getSongList()"/>
     <router-view />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
+import MusicSongList from '@/components/MusicSongList.vue';
 import $ from "jquery";
 
-@Component
-export default class Music extends Vue {}
+@Component({
+  components:{
+    Music, MusicSongList
+  }
+})
+export default class Music extends Vue {
+  getSongList():Array<any> {
+    return [
+      {
+        song: {
+          name: 'Song 1',
+          duration: '1:00',
+          genre: 'genre 1',
+          album: {
+            name: 'Album 1'
+          }
+        },
+        artist: {
+          name: 'Artist 1'
+        }
+      },
+      {
+        song: {
+          name: 'Song 1',
+          duration: '1:00',
+          genre: 'genre 1',
+          album: {
+            name: 'Album 1'
+          }
+        },
+        artist: {
+          name: 'Artist 1'
+        }
+      }
+    ]
+  }
+}
 </script>
 
 <style scoped>
 .form-control {
   border-radius: 25px;
   z-index: 900;
+}
+
+.form-control::placeholder {
+  text-align: center;
 }
 
 hr {
@@ -41,7 +82,7 @@ hr {
   padding: 1px 10px;
   font-size: 11pt;
   cursor: pointer;
-  margin: 0 2px
+  margin: 0 2px;
 }
 
 .selector:hover {
@@ -58,5 +99,4 @@ hr {
 .user-select-none {
   user-select: none;
 }
-
 </style>
